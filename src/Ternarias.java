efa632e82b32c546a2a1024a947230ebacdd8faf
jurineide <jurineidesouza@gmail.com>
2023-08-30 09:54:50 -0300

    ternaria

---
+++ src/Ternarias.java
@@ -0,0 +1,9 @@
+public class Ternarias {
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado= nota >=7 ? "aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+    }
+    
+}
